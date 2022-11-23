5c21f56a9309d44aab0152d4188883c22facd4a0
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-11-23 23:44:05 +0000

    Add rust-lang plugin

--- lua/bsteffaniak/packer.lua
+++ lua/bsteffaniak/packer.lua
@@ -45,4 +45,6 @@ return require('packer').startup(function(use)
   use("nvim-lua/popup.nvim")
   use("nvim-lua/plenary.nvim")
   use("nvim-telescope/telescope.nvim")
+
+  use("rust-lang/rust.vim")
 end)
