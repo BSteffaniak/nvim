38ab203c0d79a52c358d1b55384d139d17860e43
Braden Steffaniak <BradenSteffaniak@gmail.com>
2022-11-24 01:53:41 +0000

    Add more LSP keybindings

--- lua/bsteffaniak/set.lua
+++ lua/bsteffaniak/set.lua
@@ -34,7 +34,26 @@ local function on_attach(client, buffer)
   vim.keymap.set("n", "g0", vim.lsp.buf.document_symbol, keymap_opts)
   vim.keymap.set("n", "gW", vim.lsp.buf.workspace_symbol, keymap_opts)
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, keymap_opts)
+  vim.keymap.set("n", "<c-b>", vim.lsp.buf.definition, keymap_opts)
   vim.keymap.set("n", "ga", vim.lsp.buf.code_action, keymap_opts)
+  vim.keymap.set("n", "<M-enter>", vim.lsp.buf.code_action, keymap_opts)
+
+  -- Set updatetime for CursorHold
+  -- 300ms of no cursor movement to trigger CursorHold
+  vim.opt.updatetime = 100
+
+  -- Show diagnostic popup on cursor hover
+  local diag_float_grp = vim.api.nvim_create_augroup("DiagnosticFloat", { clear = true })
+  vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+     vim.diagnostic.open_float(nil, { focusable = false })
+    end,
+    group = diag_float_grp,
+  })
+
+  -- Goto previous/next diagnostic warning/error
+  vim.keymap.set("n", "g[", vim.diagnostic.goto_prev, keymap_opts)
+  vim.keymap.set("n", "g]", vim.diagnostic.goto_next, keymap_opts)
 end
 
 -- Configure LSP through rust-tools.nvim plugin.
