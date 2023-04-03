50af9c358fc4179cb5f36ea27d49b1805f317bdc
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-04-03 07:30:00 -0400

    Add keybinding for ripgrip\

--- lua/bsteffaniak/set.lua
+++ lua/bsteffaniak/set.lua
@@ -24,6 +24,7 @@ vim.keymap.set("n", "<Leader><Leader>", ":Files<Enter>", {noremap = true})
 vim.keymap.set("n", "<Leader>t", ":NvimTreeToggle<Enter>", {noremap = true})
 vim.keymap.set("n", "<Leader>g", ":NvimTreeFindFile<Enter>", {noremap = true})
 vim.keymap.set("n", "<Leader>c", ":NvimTreeCollapse<Enter>", {noremap = true})
+vim.keymap.set("n", "<Leader>f", ":Rg<Enter>", {noremap = true})
 
 local function on_attach(client, buffer)
   -- This callback is called when the LSP is atttached/enabled for this buffer
