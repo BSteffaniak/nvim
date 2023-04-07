333333de9e5b981040e290b63fb54863b7af7620
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-04-07 07:30:00 -0400

    Do not symlink lua-language-server because mac is dumb

--- lls.sh
+++ lls.sh
@@ -1,5 +1,7 @@
 git clone --depth 1 https://github.com/LuaLS/lua-language-server ~/.local/share/lua-language-server
 cd ~/.local/share/lua-language-server
 ./make.sh
-ln -s $HOME/.local/share/lua-language-server/bin/lua-language-server $HOME/.local/bin/lua-language-server
+echo '#!/usr/bin/env bash
+
+~/.local/share/lua-language-server/bin/lua-language-server' > ~/.local/bin/lua-language-server
 chmod +x ~/.local/bin/lua-language-server
