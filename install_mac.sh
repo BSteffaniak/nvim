09139c38e9f28eb970d14ae6d41699be6e043183
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-07-07 07:30:00 -0400

    Only brew install if package doesnt exist locally

--- install_mac.sh
+++ install_mac.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-# dependencies
-brew install ninja openjdk@17 bat gopls ccls python3-pylsp shellcheck ripgrep
+command_exists() {
+    command -v "$1"
+}
+
+[[ -z $(command_exists "ninja") ]] && brew install ninja
+[[ -z $(command_exists "bat") ]] && brew install bat
+[[ -z $(command_exists "gopls") ]] && brew install gopls
+[[ -z $(command_exists "pylsp") ]] && brew install pylsp
+[[ -z $(command_exists "spellcheck") ]] && brew install spellcheck
+[[ -z $(command_exists "rg") ]] && brew install ripgrep
 
 ./install.sh
