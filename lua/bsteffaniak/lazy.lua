8a96667c1df72d6abfe97e7cf8d6738d51d46d45
Braden Steffaniak <BradenSteffaniak@gmail.com>
2023-09-16 18:46:13 -0400

    Show full filename in status line

--- lua/bsteffaniak/lazy.lua
+++ lua/bsteffaniak/lazy.lua
@@ -109,6 +109,17 @@ require("lazy").setup({
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+    require("lualine").setup({
+      sections = {
+        lualine_c = {
+          {
+            "filename",
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1,     -- 0 = just filename, 1 = relative path, 2 = absolute path
+          },
+        },
+      },
+    }),
   },
 
   "rrethy/vim-illuminate",
