c793805723548fa32463f64af95088a7f43d5d3d
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-03-08 07:30:00 -0500

    Remove rust analyzer

--- lua/bsteffaniak/set.lua
+++ lua/bsteffaniak/set.lua
@@ -131,7 +131,8 @@ local servers = {
   gopls = {},
   html = {},
   jsonls = {},
-  rust_analyzer = {},
+  -- fsautocomplete = {},
+  -- rust_analyzer = {},
   pyright = {},
   lua_ls = {
     filetypes = { "lua" },
@@ -187,3 +188,41 @@ require("nvim-tree").setup({
     ignore = false,
   },
 })
+
+--[[
+-- Configure LSP through rust-tools.nvim plugin.
+-- rust-tools will configure and enable certain LSP features for us.
+-- See https://github.com/simrat39/rust-tools.nvim#configuration
+local rust_opts = {
+  tools = {
+    runnables = {
+      use_telescope = true,
+    },
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+    },
+  },
+
+  -- all the opts to send to nvim-lspconfig
+  -- these override the defaults set by rust-tools.nvim
+  -- see https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#rust_analyzer
+  server = {
+    -- on_attach is a callback called when the language server attachs to the buffer
+    on_attach = lsp_on_attach,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
+  },
+}
+
+require("rust-tools").setup(rust_opts)]]
