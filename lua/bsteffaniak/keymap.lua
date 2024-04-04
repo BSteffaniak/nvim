5fcd4ae161ac4ec350c045e8f61f986921907a13
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-04-03 21:08:21 -0400

    Fix qwerty vertical navigation

--- lua/bsteffaniak/keymap.lua
+++ lua/bsteffaniak/keymap.lua
@@ -54,10 +54,10 @@ end, { expr = true, noremap = true })
 vim.keymap.set({ "n", "v" }, "n", function()
   return vim.v.count > 0 and "k" or "gk"
 end, { expr = true, noremap = true })
-vim.keymap.set({ "n", "v" }, "j", function()
+vim.keymap.set({ "n", "v" }, "k", function()
   return vim.v.count > 0 and "j" or "gj"
 end, { expr = true, noremap = true })
-vim.keymap.set({ "n", "v" }, "k", function()
+vim.keymap.set({ "n", "v" }, "l", function()
   return vim.v.count > 0 and "k" or "gk"
 end, { expr = true, noremap = true })
 
