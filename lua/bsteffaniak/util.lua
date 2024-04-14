2dfe0d0854370c4ca7b08cdab31ea44b3e4e4c35
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-04-13 21:01:05 -0400

    Add dump_table util function

--- lua/bsteffaniak/util.lua
+++ lua/bsteffaniak/util.lua
@@ -185,4 +185,19 @@ M.serialize_table = function(val, name, skipnewlines, depth)
   return tmp
 end
 
+M.dump_table = function(o)
+  if type(o) == "table" then
+    local s = "{ "
+    for k, v in pairs(o) do
+      if type(k) ~= "number" then
+        k = '"' .. k .. '"'
+      end
+      s = s .. "[" .. k .. "] = " .. M.dump_table(v) .. ","
+    end
+    return s .. "} "
+  else
+    return tostring(o)
+  end
+end
+
 return M
