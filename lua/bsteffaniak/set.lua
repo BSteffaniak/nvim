c4c0ce3af235fd307f31d92ffb2b89087153616a
Braden Steffaniak <BradenSteffaniak@gmail.com>
2024-04-24 21:01:14 -0400

    Only copy to system clipboard if * register yank

--- lua/bsteffaniak/set.lua
+++ lua/bsteffaniak/set.lua
@@ -241,7 +241,9 @@ if vim.fn.has("wsl") == 1 then
   vim.api.nvim_create_autocmd("TextYankPost", {
     group = vim.api.nvim_create_augroup("Yank", { clear = true }),
     callback = function()
-      vim.fn.system("clip.exe", vim.fn.getreg('"'))
+      if vim.v.event.regname == "*" then
+        vim.fn.system("clip.exe", vim.fn.getreg('"'))
+      end
     end,
   })
 end
