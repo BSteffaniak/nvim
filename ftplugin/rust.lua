f630ef39c8a26611afd33a5b65472418a511fdab
Braden Steffaniak <BradenSteffaniak@gmail.com>
2025-06-30 07:30:00 -0400

    Remove outdated checkOnSave config

--- ftplugin/rust.lua
+++ ftplugin/rust.lua
@@ -35,10 +35,6 @@ vim.g.rustaceanvim = {
       -- to enable rust-analyzer settings visit:
       -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
       ["rust-analyzer"] = {
-        -- enable clippy on save
-        checkOnSave = {
-          command = "clippy",
-        },
         rustfmt = {
           rangeFormatting = { enable = true },
         },
